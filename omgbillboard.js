43a1a5bb8fbef1ed9dd8514e871eb9c4e9a19e6e
marymacapagal <mary.macapagal@gmail.com>
2013-05-29 01:18:57 +0900

    Create jQuery.OMGBillboard.js

---
+++ omgbillboard.js
@@ -0,0 +1,105 @@
+(function($) {
+	"use strict";
+	jQuery.fx.interval = 50;
+	$.fn.OMGBillboard = function(options) {
+	
+		var defaults = {
+			'imgWidth': 600,
+			'imgHeight': 400,
+			'numSlices': 10,
+			'sliceClass': 'slice',
+			'rotateSpeed': 500,
+			'changeSpeed': 5000,
+			'bgColor': '#000'
+			},
+			settings = $.extend({}, defaults, options);
+	
+	
+		return this.each(function() {
+			
+			var $container = $(this),
+				images = [],
+				imgIdx = 0,
+				sliceWidth = (settings.imgWidth/settings.numSlices) - 1;
+				
+			$container.find("img").each(function() {
+				images.push($(this).attr("src"));
+				$(this).hide();
+			});
+			
+			$container.css({
+				'display':'block',
+				'overflow':'hidden',
+				'background-color':settings.bgColor,
+				'height':settings.imgHeight+'px',
+				'width':settings.imgWidth+1+'px'
+			}).attr({
+				'style':settings.containerCSS
+			});
+			
+			$container[0].insertAdjacentHTML(
+				'afterbegin',
+				new Array(settings.numSlices+1).join('<div class="'+settings.sliceClass+'"></div>')
+			);
+			
+			var $slices = $container.children('div.'+settings.sliceClass);
+			
+			$slices.css({
+				'float':'left',
+				'margin':0,
+				'padding':0,
+				'height': settings.imgHeight,
+				'width': sliceWidth,
+				'max-width': sliceWidth,
+				'background':'transparent no-repeat top left',
+				'border-left':'1px solid '+settings.bgColor
+			});
+			$slices.each(function(idx) {
+				$(this).css({
+					'background-position': -idx*sliceWidth,
+					'background-image': 'url('+images[imgIdx]+')'
+				});
+			});
+			
+
+			function rotate(speed) {
+				$slices.each(function() {
+				
+					$(this).animate({
+						'width': '0',
+						'margin-left': sliceWidth/2+'px',
+						'margin-right': sliceWidth/2+'px'
+					}, {
+						duration : speed,
+						done : function() {
+							$(this).css('background-image', 'url('+images[imgIdx]+')').animate({
+								'margin-left': '0',
+								'margin-right': '0',
+								'width': sliceWidth
+							}, {
+								duration : speed
+							});
+						}
+					});
+				});
+				
+				imgIdx = imgIdx+1;
+				if (imgIdx === images.length) {imgIdx = 0;}
+				
+				setTimeout(function() {
+					rotate(speed);
+				}, settings.changeSpeed);
+			}
+			
+			
+			setTimeout(function() {
+				rotate(settings.rotateSpeed);
+			}, settings.changeSpeed);
+		
+		
+		});
+		
+	};
+	
+})(jQuery);
+
